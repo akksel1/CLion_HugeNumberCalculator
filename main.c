2994a954365c128f24d47d2cf0ea912b682911dd
KaiZyX <louka.milan@efrei.net>
2022-05-24 09:31:35 +0200

    PARTTIE 1

---
+++ main.c
@@ -0,0 +1,12 @@
+
+#include "HugeFloatOperator.h"
+#include "HugeIntOperator.h"
+#include "ShowHugeNumber.h"
+
+
+int main (void) {
+    HugeUnsignedInt* hugeNumber = createHugeUnsignedIntFromString ("123456789012345678901234567890");
+    printHugeUnsignedInt (hugeNumber);
+    hugeNumber = createHugeUnsignedIntFromString ("0000000123456789012345678901234567890");
+    printHugeUnsignedInt (hugeNumber);
+}
